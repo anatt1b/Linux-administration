6f1676a3608d4eeee58334b03eff79858abe36a7
Antti Buller <antti.buller@gmail.com>
2025-11-17 19:29:17 +0200

    testcode fix

--- myapp/pages/Streamlit_test.py
+++ myapp/pages/Streamlit_test.py
@@ -1,13 +1,16 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import plotly.express as px
 
 def main():
     st.title("Plot some data")
-    df = pd.read_csv("Oulu.csv")
-    
-    ff = px.scatter(df, x="Date", y="Oulu")
-    st.plotly_chart(ff, use_container_width=True)
+
+    df = pd.read_csv("Oulu.csv", sep=';', encoding="utf-8")
+    st.write(df.head())  # näyttää datan
+    st.write(df.columns) # näyttää sarakenimet
+
+    fig = px.scatter(df, x="DateTime", y="Maximum temperature")
+    st.plotly_chart(fig, use_container_width=True)
 
 if __name__ == "__main__":
     main()
