ab17c3d00f3fedb63ac4be1b6a0828c6d148d230
Antti Buller <antti.buller@gmail.com>
2025-11-17 18:33:58 +0200

    testcode

---
+++ myapp/pages/Streamlit_test.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd
+import plotly.expres as px
+
+def main():
+    st.title("Plot some data")
+    df = pd.read_csv("Oulu.csv")
+    
+    ff = px.scatter(df, x="Date", y="Oulu")
+    st.plotly_chart(ff, use_container_width=True)
+
+if__name__ == "__main__":
+    main()
